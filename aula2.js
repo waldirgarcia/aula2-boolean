9c2abe76fd9e1c77f1c2437bb6f28c1fbfdd462f
Waldir Neto <50805130+waldirgarcia@users.noreply.github.com>
2022-11-11 16:44:11 -0300

    JS ciência da computação, boolean
    Aula 2 JS #dio

---
+++ aula2.js
@@ -0,0 +1,5 @@
+
+/* Aplicando a ciência, boolean, saber se o número é par, aplicando  a regra resto da divisão % 2 ===0; */
+const numero = 10;
+const eNumeroPar = numero % 2 === 0;
+console.log(eNumeroPar);
