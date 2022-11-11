61a95545bdfa6d545eded4bbf354bf9df331e608
Waldir Neto <50805130+waldirgarcia@users.noreply.github.com>
2022-11-11 17:08:45 -0300

    Update aula2.js

--- aula2.js
+++ aula2.js
@@ -1,5 +1,21 @@
 
-/* Aplicando a ciência, boolean, saber se o número é par, aplicando  a regra resto da divisão % 2 ===0; */
-const numero = 10;
+/* Aplicando a ciência, boolean, saber se o número é par, aplicando  a regra resto da divisão % 2 ===0; 
+OBS: 
+um ""=""  para atribuição
+""=="" para converter texto em numero e retornar númerico
+Recomendado utilizar para comparar utilizar os "==="  */
+const numero = 0;
+
+/*se o número é divisivel por 2*/
+
 const eNumeroPar = numero % 2 === 0;
-console.log(eNumeroPar);
+
+if(numero === 0){
+    console.log('Número imparcial');
+
+}else if(eNumeroPar){
+console.log('É um número par');
+
+}else {
+console.log('É impar');
+}
